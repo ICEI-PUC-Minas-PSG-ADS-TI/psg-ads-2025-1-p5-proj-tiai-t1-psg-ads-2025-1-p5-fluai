d841f1ef3f4ee378076c61ac9bb37e1e18e0efef
Davidsb04 <caisilvasb756@hotmail.com>
2025-03-25 15:46:13 -0300

    Finalização do Cadastro e Conexão com Banco de Dados

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,13 +1,14 @@
 from controllers.userController import user_bp
 from flask import Flask
 from data.dataConfig import init_db
+from data.firebaseConfig import init_firebase
 
 
 app = Flask(__name__)
 
 init_db(app)
+init_firebase()
 
-# importando as controllers do blueeprint
 app.register_blueprint(user_bp, url_prefix="/users")
 
 
--- src/backend/data/firebaseConfig.py
+++ src/backend/data/firebaseConfig.py
@@ -3,9 +3,5 @@ from firebase_admin import credentials
 
 
 def init_firebase():
-    if not firebase_admin._apps:
-        cred = credentials.Certificate("./serviceAccountKey.json")
-        firebase_admin.initialize_app(cred)
-
-
-firebase_config = init_firebase()
+    cred = credentials.Certificate("backend/serviceAccountKey.json")
+    firebase_admin.initialize_app(cred)
