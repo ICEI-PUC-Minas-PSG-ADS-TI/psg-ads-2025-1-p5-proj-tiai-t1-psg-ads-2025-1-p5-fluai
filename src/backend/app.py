c54f64cc7ebf8991cb6b3a44cb63969e4db14190
Davidsb04 <caisilvasb756@hotmail.com>
2025-04-01 15:43:41 -0300

    Finalização do Docker e Ollama

--- src/backend/app.py
+++ src/backend/app.py
@@ -15,4 +15,4 @@ app.register_blueprint(user_bp, url_prefix="/users")
 app.register_blueprint(ollama_bp, url_prefix="/ollama")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5050)
