a42a1981d731e00e74da4263eb1663f551df423a
GabrielPen1do <gabriel.jesus.1463505@sga.pucminas.br>
2025-04-29 20:59:47 -0300

    refactor signup view

--- src/frontend/composeApp/src/commonMain/kotlin/org/example/project/ui/components/textfield/TextFieldComponent.kt
+++ src/frontend/composeApp/src/commonMain/kotlin/org/example/project/ui/components/textfield/TextFieldComponent.kt
@@ -12,12 +12,75 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import org.example.project.theme.gray
 import org.example.project.theme.gray_darker
 import org.example.project.ui.theme.PoppinsTypography
+import org.example.project.ui.utils.TextFieldBuilder
+import org.example.project.ui.utils.TextFieldState
+
+
+@Composable
+fun TextFieldComponentGeneric(
+    state: TextFieldBuilder,
+    modifier: Modifier = Modifier,
+    textLabel: String,
+    keyboardOptions: KeyboardOptions,
+    visualTransformation: VisualTransformation,
+    trailingIcon: @Composable (() -> Unit)? = null
+){
+    val errorMessage = when (val currentState = state.fieldState.value) {
+        is TextFieldState.ERROR -> currentState.erroMessage
+        else -> null
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ){
+        Text(
+            text = textLabel,
+            fontWeight = FontWeight.W400,
+            color = gray_darker,
+            style = PoppinsTypography().body2,
+            modifier = Modifier
+                .align(Alignment.Start)
+        )
+        OutlinedTextField(
+            modifier = modifier
+                .width(400.dp)
+                .padding(bottom = if (errorMessage == null) 24.dp else 0.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = gray,
+                unfocusedBorderColor = gray
+            ),
+            keyboardOptions = keyboardOptions,
+            visualTransformation = visualTransformation,
+            shape = RoundedCornerShape(12.dp),
+            value = state.textState.value,
+            isError = errorMessage != null,
+            trailingIcon = {
+                trailingIcon?.invoke()
+            },
+            onValueChange = {
+                state.textState.value = it
+                state.validate()
+            }
+        )
+
+        errorMessage?.let {
+            Text(
+                text = it,
+                color = Color.Red,
+                style = PoppinsTypography().caption,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+        }
+    }
+}
 
 @Composable
 fun TextFieldComponent(
