2b9a8fba44859a079ddefee334c0d827842c64ae
GabrielPen1do <gabriel.jesus.1463505@sga.pucminas.br>
2025-05-07 15:40:25 -0300

    ajustando identação

--- src/frontend/composeApp/src/commonMain/kotlin/org/example/project/ui/screens/signup/SignUpScreenEvent.kt
+++ src/frontend/composeApp/src/commonMain/kotlin/org/example/project/ui/screens/signup/SignUpScreenEvent.kt
@@ -3,6 +3,5 @@ package org.example.project.ui.screens.signup
 sealed interface SignUpScreenEvent {
     data object GoToAuth : SignUpScreenEvent
     data object GoToAuthBySignUp : SignUpScreenEvent
-    data class SendUserData(val name: String, val email: String, val password: String) :
-        SignUpScreenEvent
+    data class SendUserData(val name: String, val email: String, val password: String) : SignUpScreenEvent
 }
