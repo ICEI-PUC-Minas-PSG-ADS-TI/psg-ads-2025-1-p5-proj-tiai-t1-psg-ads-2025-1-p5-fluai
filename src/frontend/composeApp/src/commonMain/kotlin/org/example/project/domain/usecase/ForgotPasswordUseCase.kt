d89542892fd027c5f1a74c6da2a39262e822da0d
GabrielPen1do <gabriel.jesus.1463505@sga.pucminas.br>
2025-06-22 19:51:35 -0300

    fix

--- src/frontend/composeApp/src/commonMain/kotlin/org/example/project/domain/usecase/ForgotPasswordUseCase.kt
+++ src/frontend/composeApp/src/commonMain/kotlin/org/example/project/domain/usecase/ForgotPasswordUseCase.kt
@@ -1,13 +1,4 @@
 package org.example.project.domain.usecase
 
-import org.example.project.data.dto.Email
-import org.example.project.data.dto.ForgotPasswordResponseDto
-import org.example.project.domain.repository.ForgotPasswordRepository
-
-class ForgotPasswordUseCase(
-    private val repository: ForgotPasswordRepository
-) {
-    suspend operator fun invoke(email: String): Result<ForgotPasswordResponseDto> {
-        return repository.forgotPassword(Email(email))
-    }
+interface ForgotPasswordUseCase {
 }
